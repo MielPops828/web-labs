4e69b4fa628fc3100aee00ac54add8861975b0ef
MielPops828 <merku0103@mail.ru>
2025-04-17 16:10:41 +0400

    add lab5

---
+++ lab5/lab2/src/main/java/com/ssau/lab2/GlobalException.java
@@ -0,0 +1,14 @@
+package com.ssau.lab2;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
